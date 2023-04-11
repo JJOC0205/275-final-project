1d7735a6eb458449eea852dc1d28f0650d48a62d
rrachelr <rachrobins1@gmail.com>
2023-04-11 12:33:55 -0400

    npm run start is working

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">Test</header>
+        </div>
+    );
+}
+export default App;
