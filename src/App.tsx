70a1355f45a5d52712cac9f431bd90034a3a55df
Shreya <shrey@udel.edu>
2023-04-17 10:22:53 -0400

    added names

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">Test</header>
+            <h1>Jon O' Connell, Rachel Robins, Ani Naredla, Shreya Pamulapati</h1>
         </div>
     );
 }
