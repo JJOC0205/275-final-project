27473b0f6742d7d6f29ebf0ef55925f700f8532e
Shreya <shrey@udel.edu>
2023-04-18 14:34:11 -0400

    added names

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 
 function App(): JSX.Element {
-    const [movies] = useState<Movie>(theMovie);
     return (
-        <div className="App">
+        <div className = "App">
             <header className="App-header">Test</header>
-            <h1>Jon O' Connell, Rachel Robins, Ani Naredla, Shreya Pamulapati</h1>
-            <ShowMovieDetails movie={movies}></ShowMovieDetails>
+            <h1>Jon O' Conell, Rachel Robins, Ani Naredla, Shreya Pamulapati</h1>
         </div>
     );
 }
