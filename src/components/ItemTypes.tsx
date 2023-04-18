272e4b7d5305325639028b7ea93fa37421ca77b5
Ani Naredla <124926574+aninaredla@users.noreply.github.com>
2023-04-18 15:05:51 -0400

    Add files via upload
    itemtypes for dnd test

---
+++ src/components/ItemTypes.tsx
@@ -0,0 +1,11 @@
+export const ItemTypes = {
+    BOX: 'box',
+    MOVIE: 'movie'
+}
+
+// export const ItemTypes = {
+//     FOOD: 'food',
+//     GLASS: 'glass',
+//     PAPER: 'paper',
+//   }
+  
