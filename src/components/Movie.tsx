d7527d2a7a43bd4865caa2041740d986ffe129aa
Ani Naredla <124926574+aninaredla@users.noreply.github.com>
2023-04-19 09:13:29 -0400

    Add files via upload

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -2,50 +2,51 @@ import { DragSourceMonitor, useDrag } from "react-dnd";
 import React from "react";
 import { ItemTypes } from "./ItemTypes";
 
-export const testMovies: MovieProps[] = [
-    {
-        title: "The Shawshank Redemption",
-        released: 1994,
-        runtime: 142,
-        watched: true,
-        description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-        rating: 9.3,
-    },
-    {
-        title: "The Godfather",
-        released: 1972,
-        runtime: 175,
-        watched: true,
-        description: "An organized crime dynasty's aging patriarch transfers control of his clandestine empire to his reluctant son.",
-        rating: 9.2,
-      },
-      {
-        title: "The Dark Knight",
-        released: 2008,
-        runtime: 152,
-        watched: true,
-        description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-        rating: 9.0,
-      },
-      {
-        title: "12 Angry Men",
-        released: 1957,
-        runtime: 96,
-        watched: false,
-        description: "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence.",
-        rating: 8.9,
-      },
-      {
-        title: "Schindler's List",
-        released: 1993,
-        runtime: 195,
-        watched: false,
-        description: "In German occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
-        rating: 8.9,
-      },
-];
+// export const testMovies: MovieProps[] = [
+//   {
+//       title: "The Shawshank Redemption",
+//       released: 1994,
+//       runtime: 142,
+//       watched: true,
+//       description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+//       rating: 9.3,
+//   },
+//   {
+//       title: "The Godfather",
+//       released: 1972,
+//       runtime: 175,
+//       watched: true,
+//       description: "An organized crime dynasty's aging patriarch transfers control of his clandestine empire to his reluctant son.",
+//       rating: 9.2,
+//     },
+//     {
+//       title: "The Dark Knight",
+//       released: 2008,
+//       runtime: 152,
+//       watched: true,
+//       description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+//       rating: 9.0,
+//     },
+//     {
+//       title: "12 Angry Men",
+//       released: 1957,
+//       runtime: 96,
+//       watched: false,
+//       description: "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence.",
+//       rating: 8.9,
+//     },
+//     {
+//       title: "Schindler's List",
+//       released: 1993,
+//       runtime: 195,
+//       watched: false,
+//       description: "In German occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
+//       rating: 8.9,
+//     },
+// ];
 
 export interface MovieProps {
+    id: number;
     title: string;
     released: number;
     runtime: number;
@@ -54,7 +55,7 @@ export interface MovieProps {
     rating: number;
 }
 
-function Movie({title, released, runtime, watched, description, rating}: MovieProps) {
+function Movie({title, released, runtime, watched, description, rating}: MovieProps): JSX.Element {
     const [{isDragging}, drag] = useDrag({
         type: ItemTypes.MOVIE,
         item: {title: title, released: released, runtime: runtime, watched: watched, description: description, rating: rating},
@@ -71,7 +72,7 @@ function Movie({title, released, runtime, watched, description, rating}: MoviePr
             <p>Watched: {watched.toString()}</p>
             <p>Description: {description}</p>
             <p>Rating out of 10: {rating}</p>
-            <div ref={drag}/>
+            <div/>
             {/* <div><button>CLICK ME</button></div> */}
         </div>
       )
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -6,7 +6,55 @@ import { ItemTypes } from "./ItemTypes";
 import Movie from "./Movie";
 import './UserList.css';
 
-export const userMoviesList = [];
+//export const userMoviesList:MovieProps[] = [];
+
+export const testMovies: MovieProps[] = [
+    {
+        id: 0,
+        title: "The Shawshank Redemption",
+        released: 1994,
+        runtime: 142,
+        watched: true,
+        description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+        rating: 9.3,
+    },
+    {
+        id: 1,
+        title: "The Godfather",
+        released: 1972,
+        runtime: 175,
+        watched: true,
+        description: "An organized crime dynasty's aging patriarch transfers control of his clandestine empire to his reluctant son.",
+        rating: 9.2,
+      },
+      {
+        id: 2,
+        title: "The Dark Knight",
+        released: 2008,
+        runtime: 152,
+        watched: true,
+        description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+        rating: 9.0,
+      },
+      {
+        id: 3,
+        title: "12 Angry Men",
+        released: 1957,
+        runtime: 96,
+        watched: false,
+        description: "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence.",
+        rating: 8.9,
+      },
+      {
+        id: 4,
+        title: "Schindler's List",
+        released: 1993,
+        runtime: 195,
+        watched: false,
+        description: "In German occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
+        rating: 8.9,
+      },
+];
 
 interface UserListExport {
     userMovies: MovieProps[];
@@ -14,28 +62,28 @@ interface UserListExport {
 
 export function UserList(): JSX.Element {
 
-    const [userMovies, setUserMovies] = useState<MovieProps[]>(userMoviesList);
-    function updateUserMovies(movie: MovieProps) {
-        setUserMovies([...userMovies, movie])
+    const [userMovies, setUserMovies] = useState<MovieProps[]>([]);
+    function updateUserMovies(newMovie: MovieProps) {
+        const movieFilter = testMovies.filter((movie) => newMovie.title === movie.title);
+        setUserMovies([...userMovies, movieFilter[0]]);
         console.log(userMovies.length);
-        userMovies.map((movie) => console.log(movie.title));
+        // userMovies.map((movie) => console.log(movie.title));
     }
 
-    const [{isOver}, drop] = useDrop(() => ({
+    const [{isOver}, drop] = useDrop({
         accept: ItemTypes.MOVIE,
-        drop: (item: MovieProps) => updateUserMovies(item),
+        drop: (item: MovieProps, monitor) => updateUserMovies(item),
         collect: (monitor) => ({
             isOver: monitor.isOver(),
-            canDrop: monitor.canDrop()
         }),
-    }),)
+    })
 
 
     return (
-        <div ref={drop} style={{ backgroundColor: isOver ? 'red' : 'white', width: "510px", height:"1000px", border: "1px solid#000", overflow: "auto" }}>
+        <div ref={drop} style={{ backgroundColor: isOver ? 'red' : 'white', width: "510px", height:"1000px", border: "1px solid#000"}}>
             {userMovies.map((movie) => {
-                return <div className="ListItem">
-                    <Movie title={movie.title} released={movie.released} runtime={movie.released} watched={movie.watched} description={movie.description} rating={movie.rating}></Movie>
+                return <div className="ListItem" key={movie.id}>
+                    <Movie id={movie.id} title={movie.title} released={movie.released} runtime={movie.runtime} watched={movie.watched} description={movie.description} rating={movie.rating}></Movie>
                 </div>
             })}
         </div>
