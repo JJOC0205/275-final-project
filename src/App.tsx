4a3b2bfe6e673dcdc27a186f19f691ec5f7c0164
rrachelr <rachrobins1@gmail.com>
2023-04-19 00:15:53 -0400

    implemented movie json file in app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,13 @@
 import React, { useState } from "react";
+import movies from "./data/movies.json";
 import { CentralItemList } from "./components/CentralItemList";
 import { Movie } from "./interfaces/movie";
 
-const MOVIES = [
-    {
-        title: "Pride and Prejudice",
-        released: 2005,
-        runtime: 60,
-        watched: true,
-        description: "test",
-        rating: 10,
-        poster: "https://m.media-amazon.com/images/I/81Uk9cyj1-L.jpg"
-    },
-    {
-        title: "Test Movie",
-        released: 2005,
-        runtime: 60,
-        watched: true,
-        description: "test",
-        rating: 10,
-        poster: "https://m.media-amazon.com/images/I/81Uk9cyj1-L.jpg"
-    },
-    {
-        title: "Star Wars: All of Them",
-        released: 2005,
-        runtime: 60,
-        watched: true,
-        description: "test",
-        rating: 10,
-        poster: "https://m.media-amazon.com/images/I/81Uk9cyj1-L.jpg"
-    },
-    {
-        title: "Boss Baby",
-        released: 2005,
-        runtime: 60,
-        watched: true,
-        description: "test",
-        rating: 10,
-        poster: "https://m.media-amazon.com/images/I/81Uk9cyj1-L.jpg"
-    }
-];
+const MOVIES = movies.map(
+    (movie): Movie => ({
+        ...movie
+    })
+);
 
 function App(): JSX.Element {
     const [movies] = useState<Movie[]>(MOVIES);
