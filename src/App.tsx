f7a6dc1dd156a8fd089eb38a30f222dccf647e78
Ani Naredla <124926574+aninaredla@users.noreply.github.com>
2023-04-19 09:12:40 -0400

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from 'react-dnd/dist/core';
 import Movie from './Movie';
 import UserList from './UserList';
-import { testMovies } from './Movie';
+import { testMovies } from './UserList';
 
 function App() {
   return (
@@ -12,12 +12,10 @@ function App() {
     <div className="App" style={{display: "flex"}}>
       <DndProvider backend={HTML5Backend}>
         <div style={{flex: "1"}}>
-          {testMovies.map((movie) => (<Movie title={movie.title} released={movie.released} runtime={movie.runtime} watched={movie.watched} description={movie.description} rating={movie.rating}></Movie>))}
+          {testMovies.map((movie) => (<Movie id={movie.id} title={movie.title} released={movie.released} runtime={movie.runtime} watched={movie.watched} description={movie.description} rating={movie.rating}></Movie>))}
         </div>
         <div style={{flex: "1"}}>
-          {/* <div> */}
           <UserList></UserList>
-          {/* </div> */}
         </div>
       </DndProvider>
     </div>
