e319f3cef52a83bc6f00424aefbf9da362178ae6
Ani Naredla <124926574+aninaredla@users.noreply.github.com>
2023-04-20 14:29:08 -0400

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from 'react-dnd/dist/core';
@@ -7,15 +7,18 @@ import UserList from './UserList';
 import { testMovies } from './UserList';
 
 function App() {
+  const [userMode, setUserMode] = useState<boolean>(false);
   return (
     <>
     <div className="App" style={{display: "flex"}}>
       <DndProvider backend={HTML5Backend}>
+        <button onClick={() => setUserMode(!userMode)}>Activate User List</button>
         <div style={{flex: "1"}}>
           {testMovies.map((movie) => (<Movie id={movie.id} title={movie.title} released={movie.released} runtime={movie.runtime} watched={movie.watched} description={movie.description} rating={movie.rating}></Movie>))}
         </div>
         <div style={{flex: "1"}}>
-          <UserList></UserList>
+          {/* <UserList></UserList> */}
+          {userMode && <UserList></UserList>}
         </div>
       </DndProvider>
     </div>
