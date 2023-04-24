d463743a73a7b9cfa8fe69838ac076ca1adbb435
Anirudh Naredla <anirudhn@udel.edu>
2023-04-24 14:46:45 -0400

    fixed usedrag issue, REVIEW BEFORE MERGING

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { MOVIES } from "./data/MovieList";
 import { CentralItemList } from "./components/CentralItemList";
 import { Movie } from "./interfaces/movie";
 import "./App.css";
+import UserList from "./components/UserList";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd/dist/core";
 
 function App(): JSX.Element {
     const [movies] = useState<Movie[]>(MOVIES);
@@ -12,7 +15,10 @@ function App(): JSX.Element {
                 <h3>Movie World</h3>
                 Jon OConell, Rachel Robins, Ani Naredla, Shreya Pamulapati
             </header>
-            <CentralItemList movies={movies}></CentralItemList>
+            <DndProvider backend={HTML5Backend}>
+                <CentralItemList movies={movies}></CentralItemList>
+                <UserList></UserList>
+            </DndProvider>
         </div>
     );
 }
