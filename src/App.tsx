26bde99d36883cb1f065fb41efe06c240fbd2ab2
Anirudh Naredla <anirudhn@udel.edu>
2023-04-24 13:09:55 -0400

    testing clone repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import movies from "./data/movies.json";
 import { CentralItemList } from "./components/CentralItemList";
 import { Movie } from "./interfaces/movie";
