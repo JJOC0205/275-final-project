6f5f3c5e5dcb55d4e05177540b68fd9099ff210a
rrachelr <rachrobins1@gmail.com>
2023-05-04 15:04:26 -0400

    replaced app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-// import { useState } from "react";
+import { useState } from "react";
 import { CentralItemList } from "./components/CentralItemList";
 import "./App.css";
 import { UserList } from "./components/UserList";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd/dist/core";
 import { SuperList } from "./components/SuperList";
+import { UserSelect } from "./components/UserSelect";
+import { USERS } from "./data/UsersList";
+import { User } from "./interfaces/user";
 
 function App(): JSX.Element {
     // const [showUser, setShowUser] = useState<boolean>(false);
+    const [users] = useState<User[]>(USERS);
+    const [currUser, setUser] = useState<User>(users[0]);
     return (
         <div className="App">
             <header className="App-header">
@@ -19,7 +24,12 @@ function App(): JSX.Element {
                 <hr></hr>
                 <UserList></UserList>
                 <hr></hr>
-                <SuperList></SuperList>
+                <SuperList user={currUser}></SuperList>
+                <UserSelect
+                    users={users}
+                    user={currUser}
+                    setUser={setUser}
+                ></UserSelect>
             </DndProvider>
         </div>
     );
