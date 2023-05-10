dab8199cb326afaacd2868fe06a547114eab7a9b
Anirudh Naredla <anirudhn@udel.edu>
2023-05-09 22:20:37 -0400

    fixed linting issue for previous deployment

--- src/tests/SuperAddUser.test.tsx
+++ src/tests/SuperAddUser.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { SuperAddUser } from "../components/SuperAddUser";
